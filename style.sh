459c19efc3abe72acde40d5bee12bcfd85d06973
datttrian <datttrian@gmail.com>
2023-09-05 11:04:27 -0700

    Add Rmarkdown files

--- style.sh
+++
@@ -1,4 +0,0 @@
-set -xe
-isort introduction-to-python intermediate-python --check-only
-flake8 introduction-to-python intermediate-python --show-source
-pylint --disable=R0801 introduction-to-python intermediate-python
